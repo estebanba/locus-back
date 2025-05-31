6630bf34b42c772eba5a5c79eef5c16b24fe0157
estebanba <34803146+estebanba@users.noreply.github.com>
2025-05-31 20:00:45 +0200

    new strategy fix 47

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,25 @@
 module.exports = {
-  apps: [{
-    name: 'locus-back',
-    script: 'dist/app.js',
-    cwd: '/var/www/locus-backend',
-    interpreter: '/home/deploy/.nvm/versions/node/v22.11.0/bin/node',
-    env_production: {
-      NODE_ENV: 'production'
+  apps: [
+    {
+      name: "locus-back",
+      script: "/var/www/locus-back/dist/server.js",
+      instances: "max",
+      exec_mode: "cluster",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 7001,
+        CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+        CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+        CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+      },
+      error_file: "/var/www/locus-back/logs/error.log",
+      out_file: "/var/www/locus-back/logs/out.log",
+      restart_delay: 1000,
+      kill_timeout: 3000,
+      cwd: "/var/www/locus-back",
     },
-    error_file: '/var/www/locus-backend/logs/error.log',
-    out_file: '/var/www/locus-backend/logs/out.log',
-    merge_logs: true,
-    log_date_format: 'YYYY-MM-DD HH:mm Z'
-  }]
+  ],
 }; 
